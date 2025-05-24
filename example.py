6d3a9655b57465ea9a6b8b1eef6f77056fdfa8f1
Pawni Yadav <145229487+Pawnios@users.noreply.github.com>
2025-05-25 03:19:28 +0530

    Update example.py

--- example.py
+++ example.py
@@ -1,6 +1,6 @@
-   import pandas as pd
-   import matplotlib.pyplot as plt
-   import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import streamlit as st
 
    # Streamlit Interface
    st.title("CSV Data Visualizer")
